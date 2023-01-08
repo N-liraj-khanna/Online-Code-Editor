69330845659f4ca4a121be0ab5d7b291a05dfef8
N Liraj Khanna <lirajkhanna2002@gmail.com>
2023-01-08 19:07:40 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+express = require('express');
+app=express();
+
+app.get('/', (req, res) => {
+  return res.send('Welcome to code editor');
+})
+
+app.listen(process.env.PORT || 5000, ()=>{
+  console.log(`Server up and running on port ${process.env.PORT || 5000}`);
+})
